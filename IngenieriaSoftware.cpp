b19f815bdc4ab7b373fe3b9f7da62ca683eb0742
LuisTorres19 <73291737+LuisTorres19@users.noreply.github.com>
2023-01-15 23:36:18 +0100

    player

--- IngenieriaSoftware.cpp
+++ IngenieriaSoftware.cpp
@@ -12,7 +12,7 @@ int aliveEnemies(bool* enemies, int size);
 int main()
 {
 	//init
-	int playerPos = 75;
+	int playerPos = 55;
 	bool playerDir = true;
 	int mapSize = 120;
 	char* map = new char[mapSize + 1];
